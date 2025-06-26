249f7e73e25eb35b0195b91a4f414c0b94bd95ca
Azizbek <132741338+xudoyberdiyev-dev@users.noreply.github.com>
2025-06-27 00:14:43 +0500

    sa

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -24,7 +24,7 @@ export default function Navbar() {
 
     return (
         <nav className="w-full backdrop-blur-md bg-white/70 border-b border-gray-200 px-5 py-4 flex justify-between items-center sticky top-0 z-50 shadow-sm">
-            <Link href="/" className="text-[20px] font-extrabold tracking-wide text-yellow-500 drop-shadow-sm">
+            <Link href="/category" className="text-[20px] font-extrabold tracking-wide text-yellow-500 drop-shadow-sm">
                 Mini Shop
             </Link>
 
