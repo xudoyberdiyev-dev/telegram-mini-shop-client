819379429017379f4e587fb480c06a350ec2cc3a
Azizbek <132741338+xudoyberdiyev-dev@users.noreply.github.com>
2025-06-27 00:47:13 +0500

    category

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -1,3 +1,4 @@
+
 export default function Page() {
     return(
         <div className={'w-full h-[100vh] py-3 p-2'}>
@@ -6,32 +7,33 @@ export default function Page() {
                     Bo'limlar
                 </h1>
             </div>
+            <div className="grid mt-5 grid-cols-2 gap-4">
+                <div className="bg-white rounded-xl shadow-md p-1 pb-1 flex flex-col items-center">
+                    {/* Rasm */}
+                    <img
+                        className="w-full bg-red-300 h-32 object-cover rounded-lg"
+                    />
 
+                    {/* Nomi */}
+                    <h2 className="text-lg font-semibold text-yellow-900 mt-1 text-center">
+                        salom
+                    </h2>
 
-            <div
-                className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-                <a href="#">
-                    <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt=""/>
-                </a>
-                <div className="p-5">
-                    <a href="#">
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy
-                            technology acquisitions 2021</h5>
-                    </a>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise
-                        technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                    <a href="#"
-                       className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Read more
-                        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
-                             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                  d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                        </svg>
-                    </a>
+                    {/* Tugmalar */}
+                    <div className="flex gap-2 mt-2">
+                        <button
+                            className="px-3 py-1.5 text-sm font-semibold  shadow-md shadow-black-200 text-yellow-900 rounded hover:bg-yellow-500-600"
+                        >
+                            Tahrirlash
+                        </button>
+                        <button
+                            className="px-3 py-1.5 text-sm font-semibold  shadow-md shadow-black-200 text-yellow-900 rounded hover:bg-yellow-500-600"
+                        >
+                            O‘chirish
+                        </button>
+                    </div>
                 </div>
             </div>
-
         </div>
     )
 }
